f409695651a555c5f56f3d617abd718fe3065402
Michael Schnell <1856319+michael-schnell@users.noreply.github.com>
2019-01-17 10:00:13 +0100

    Create eventstore.sh

---
+++ eventstore.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+EVENTSTORE_ARCHIVE=EventStore-OSS-Ubuntu-18.04-v4.1.1-hotfix1
+curl https://eventstore.org/downloads/$EVENTSTORE_ARCHIVE.tar.gz --output eventstore.tar.gz
+tar -xzf eventstore.tar.gz
+cd $EVENTSTORE_ARCHIVE
+MY_IP=echo "${HOSTNAME:3}" | sed 's/-/./g'
+sudo ./run-node.sh --ext-ip=$MY_IP --ext-http-port=80 --ext-http-prefixes="http://*/" --add-interface-prefixes=false
